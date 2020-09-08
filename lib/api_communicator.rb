75ad69a97853c42ff690bb57045815aa85a7f370
tcharfi66 <charfitej6@gmail.com>
2020-09-08 12:30:47 -0400

    status

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,15 +2,31 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character_name)
+def get_character_movies_from_api(character_name) #return array of hashes (each hash is a movie)
   #make the web request
+
+  #This array of hashes is what gets passed to the `print_movies` method. 
+  #This is the method in which you will iterate over that array of hashes to `puts` out movie information to the terminal.
 
 
   response_string = RestClient.get('http://swapi.dev/api/people')
   response_hash = JSON.parse(response_string)
 
+    api_urls = [] 
+    
+  response_hash.each do |character|
+   if character[:name] == character_name
+      api_url << character[:films]
+   end
+
+
+   #make web request with each element from api_url 
+
+  end
+
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
+
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
   # return value of this method should be collection of info about each film.
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,14 @@
+# Define a `welcome` method that `puts` out a friendly greeting. 
+# Then, define the `get_character_from_user` method to ask the user for a characters name and capture that input. 
+# The return value of this method should be the captured user's input.
+
 def welcome
   # puts out a welcome message here!
+  puts "Welcome!"
 end
 
 def get_character_from_user
   puts "please enter a character name"
+  return gets.chomp.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end
